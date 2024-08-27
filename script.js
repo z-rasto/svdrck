d519ec31544a4eda1d142c038e2e37a240b50390
Rastislav Zlacky <rasto.zlacky@gmail.com>
2024-08-27 22:07:27 +0200

    svdrck

--- script.js
+++ script.js
@@ -2,11 +2,14 @@ var scale = 0;
 
 window.addEventListener('scroll', function () {
     const scrollPosition = window.scrollY;
-    console.log('Scroll Position:', scrollPosition); // This will help confirm if the event is firing.
+    // console.log('Scroll Position:', scrollPosition); // This will help confirm if the event is firing.
 
     const maxScrollY = document.documentElement.scrollHeight - window.innerHeight;
-    console.log(document.documentElement.scrollHeight)
+    // console.log(document.documentElement.scrollHeight)
     const airplane = document.querySelector('.airplane');
+    const bus = document.querySelector('.bus');
+    const jozi = document.querySelector('.jozi');
+    const adka = document.querySelector('.adka');
     const animationContainer = document.querySelector('.animation-container');
     const landing = maxScrollY / 4;
     const maxFlight = maxScrollY - landing;
@@ -18,22 +21,50 @@ window.addEventListener('scroll', function () {
         animationContainer.style.position = 'relative';
         airplane.style.top = `50%`;
         airplane.style.transform = `translateX(-50%) translateY(-50%) rotate(180deg) scale(${scale / 8500})`;
+        bus.style.visibility = `hidden`
+        if (scrollPosition >= maxAirplaneHeight - (maxAirplaneHeight / 8)) {
+            const oS = (scrollPosition - (maxAirplaneHeight - (maxAirplaneHeight / 8))) / 1000;
+            jozi.style.transform = `translateX(-50%) translateY(-50%) scale(${oS})`
+            adka.style.transform = `translateX(-50%) translateY(-50%) scale(${oS})`
+        } else {
+            jozi.style.transform = `translateX(-50%) translateY(-50%) scale(0)`
+            adka.style.transform = `translateX(-50%) translateY(-50%) scale(0)`
+        }
     } else if (scrollPosition > maxAirplaneHeight && scrollPosition < (maxFlight - maxAirplaneHeight / 5)) {
         scale = maxFlight - scrollPosition;
         airplane.style.position = `fixed`;
         animationContainer.style.position = 'relative';
         airplane.style.top = `50%`;
         airplane.style.transform = `translateX(-50%) translateY(-50%) rotate(180deg) scale(${scale / 8500})`;
+        bus.style.visibility = `hidden`
+        const aaa = scrollPosition - (maxAirplaneHeight - (maxAirplaneHeight / 8));
+        const bbb = (scrollPosition - maxAirplaneHeight) * 2;
+        const oS = (aaa - bbb) / 1000;
+        if (oS > 0) {
+            jozi.style.transform = `translateX(-50%) translateY(-50%) scale(${oS})`
+            adka.style.transform = `translateX(-50%) translateY(-50%) scale(${oS})`
+        } else {
+            jozi.style.transform = `translateX(-50%) translateY(-50%) scale(0)`
+            adka.style.transform = `translateX(-50%) translateY(-50%) scale(0)`
+        }
     } else if (scrollPosition < 0.68 * maxScrollY) {
         scale = maxAirplaneHeight / 5
         airplane.style.position = `fixed`;
         animationContainer.style.position = 'relative';
         airplane.style.top = `50%`;
         airplane.style.transform = `translateX(-50%) translateY(-50%) rotate(180deg) scale(${scale / 8500})`;
+        bus.style.visibility = `hidden`
+
+        jozi.style.transform = `translateX(-50%) translateY(-50%) scale(0)`
+        adka.style.transform = `translateX(-50%) translateY(-50%) scale(0)`
     } else {
         airplane.style.transform = `translateX(-50%) translateY(-50%) rotate(180deg) scale(${maxAirplaneHeight / 5 / 8500})`;
         airplane.style.position = `absolute`;
         airplane.style.top = `1682%`;
         animationContainer.style.position = `unset`
+        bus.style.visibility = `visible`
+
+        jozi.style.transform = `translateX(-50%) translateY(-50%) scale(0)`
+        adka.style.transform = `translateX(-50%) translateY(-50%) scale(0)`
     }
 });
